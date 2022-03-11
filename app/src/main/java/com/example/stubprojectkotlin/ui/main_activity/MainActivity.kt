390f54e4bbac947b5fdb2abff8843ee59e6dee4e
hnalgorepublic <Husnain.nasir@algorepublic.com>
2022-03-11 18:39:59 +0500

    isContactExist file changes

--- app/src/main/java/com/example/stubprojectkotlin/ui/main_activity/MainActivity.kt
+++ app/src/main/java/com/example/stubprojectkotlin/ui/main_activity/MainActivity.kt
@@ -1,22 +1,30 @@
 package com.example.stubprojectkotlin.ui.main_activity
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.TextView
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import com.example.stubprojectkotlin.BaseActivity
-import com.example.stubprojectkotlin.background_jobs.BackgroundJobs
 import com.example.stubprojectkotlin.databinding.ActivityMainBinding
 import com.example.stubprojectkotlin.db.entites.User
 import com.example.stubprojectkotlin.network.MainViewModel
 import com.example.stubprojectkotlin.utils.LocationManager
 import com.example.stubprojectkotlin.utils.Status.*
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.*
+import com.dynamsoft.dbr.*;
+import com.dynamsoft.dce.CameraEnhancer
+import com.dynamsoft.dce.CameraEnhancerException
+
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity() {
+
+    var reader: BarcodeReader? = null
+    var mCameraEnhancer: CameraEnhancer? = null
+
 
     private lateinit var activityMainBinding: ActivityMainBinding
 
@@ -50,13 +58,13 @@ class MainActivity : BaseActivity() {
         activityMainBinding.getMalls.setOnClickListener {
 //            mainViewModel.getMalls()
 
-            val workManagerService = BackgroundJobs(applicationContext)
-
-            workManagerService.createOneTimeWorkRequest()
-            workManagerService.getWorkInfoLiveData().observe(this , {
-                val status: String = it.state.name
-                Toast.makeText(this,status, Toast.LENGTH_SHORT).show()
-            })
+//            val workManagerService = BackgroundJobs(applicationContext)
+//
+//            workManagerService.createOneTimeWorkRequest()
+//            workManagerService.getWorkInfoLiveData().observe(this , {
+//                val status: String = it.state.name
+//                Toast.makeText(this,status, Toast.LENGTH_SHORT).show()
+//            })
         }
 
         mainViewModel.getMallsLive().observe(this, {
@@ -71,6 +79,66 @@ class MainActivity : BaseActivity() {
 
 
 
+        try {
+            // Create an instance of Dynamsoft Barcode Reader.
+            reader = BarcodeReader()
+
+            // Initialize license for Dynamsoft Barcode Reader.
+            // The organization id 200001 here will grant you a public trial license good for 7 days. Note that network connection is required for this license to work.
+            // If you want to use an offline license, please contact Dynamsoft Support: https://www.dynamsoft.com/company/contact/
+            // You can also request a 30-day trial license in the customer portal: https://www.dynamsoft.com/customer/license/trialLicense?product=dbr&utm_source=installer&package=android
+            val dbrParameters = DMDLSConnectionParameters()
+            dbrParameters.organizationID = "200001"
+            reader!!.initLicenseFromDLS(
+                dbrParameters
+            ) { isSuccessful, e ->
+                runOnUiThread {
+                    if (!isSuccessful) {
+                        e.printStackTrace()
+                        showErrorDialog(e.message!!)
+                    }
+                }
+            }
+        } catch (e: BarcodeReaderException) {
+            e.printStackTrace()
+        }
+
+        // Create a listener to obtain the recognized barcode results.
+
+        // Create a listener to obtain the recognized barcode results.
+        val mTextResultCallback =
+            TextResultCallback { i, textResults, userData ->
+                // Obtain the recognized barcode results and display.
+                runOnUiThread {
+
+                    activityMainBinding.cameraView.alpha = 0.1f
+                    showResult(textResults)
+                }
+            }
+
+        // Create an instance of Dynamsoft Camera Enhancer for video streaming.
+
+        // Create an instance of Dynamsoft Camera Enhancer for video streaming.
+        mCameraEnhancer = CameraEnhancer(this@MainActivity)
+        mCameraEnhancer!!.cameraView = activityMainBinding.cameraView
+
+        // Bind the Camera Enhancer instance to the Barcode Reader instance.
+
+        // Bind the Camera Enhancer instance to the Barcode Reader instance.
+        reader!!.setCameraEnhancer(mCameraEnhancer!!)
+
+        // Make this setting to get the result. The result will be an object that contains text result and other barcode information.
+
+        // Make this setting to get the result. The result will be an object that contains text result and other barcode information.
+        try {
+            reader!!.setTextResultCallback(mTextResultCallback, null)
+        } catch (e: BarcodeReaderException) {
+            e.printStackTrace()
+        }
+
+
+
+
 //        GlobalScope.launch {
 //            val result = mainViewModel.getUsersFromDB()
 //
@@ -78,6 +146,53 @@ class MainActivity : BaseActivity() {
 //        }
 
 
+    }
+
+    override fun onResume() {
+        // Start video barcode reading
+        try {
+            mCameraEnhancer!!.open()
+        } catch (e: CameraEnhancerException) {
+            e.printStackTrace()
+        }
+        reader!!.startScanning()
+        super.onResume()
+    }
+
+    override fun onPause() {
+        // Stop video barcode reading
+        try {
+            mCameraEnhancer!!.close()
+        } catch (e: CameraEnhancerException) {
+            e.printStackTrace()
+        }
+        reader!!.stopScanning()
+        super.onPause()
+    }
+
+
+
+
+    private fun showResult(results: Array<TextResult>?) {
+        if (results != null && results.size > 0) {
+            var strRes = ""
+            for (i in results.indices) strRes += """
+     ${results[i].barcodeText}
+     
+     """.trimIndent()
+           activityMainBinding.tvRes.text = strRes
+            activityMainBinding.cameraView.alpha = 1f
+        } else {
+            activityMainBinding.tvRes.text = ""
+        }
+    }
+
+    private fun showErrorDialog(message: String) {
+        val dialog = AlertDialog.Builder(this)
+        dialog.setTitle("Verification Failed")
+            .setPositiveButton("OK", null)
+            .setMessage(message)
+            .show()
     }
 
      private fun showUser(users : List<User>) {
